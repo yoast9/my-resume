db69a833c62b2059c978bc7a5c140b655835d391
Riley <rspehler@udel.edu>
2017-11-09 13:53:36 -0500

    added new jing

--- songbase.py
+++ songbase.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    # return '<h1>hello world </h1>'
+    return render_template('index.html')
+
+@app.route('/songs')
+def get_all_songs():
+    songs = [
+        'song 1',
+        'song 2',
+        'song 3'
+    ]
+    return render_template('songs.html',songs=songs)
+
 @app.route('/user/<string:name>')
 def get_user():
-    return '<h1>hello %s your age is %d/h1>' % (name, s)
+    #return '<h1>hello %s your age is %d/h1>' % (name, s)
+    return render_template('user.html',user_name=name)
 if __name__ == '__main__':
     """app.run(host='0.0.0.0',port=2000)"""
     app.run(debug=True)
